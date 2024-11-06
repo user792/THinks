78a8f9dd12db47da596c454b84d8a40bbf823059
viki <viki.lehtonen@edu.hyria.fi>
2024-11-06 10:36:33 +0200

    Update SuperMarkoBrothers.py

--- super marko brothers/SuperMarkoBrothers.py
+++ super marko brothers/SuperMarkoBrothers.py
@@ -3,21 +3,24 @@ import pygame
 pygame.init()
 
 class Attribute:
-    def __init__(self, y_velocity:float, x_velocity:float, on_ground:bool, speed:float, max_speed:float):
+    def __init__(self, y_velocity:float, x_velocity:float, on_ground:bool, speed:float, max_speed:float,character):
         self.y_velocity = y_velocity
         self.x_velocity = x_velocity
         self.on_ground = on_ground
         self.speed = speed
         self.max_speed = max_speed
+        self.character = character
 
     def __repr__(self):
         return (f"Attribute(y_velocity={self.y_velocity}, x_velocity={self.x_velocity}, "
                 f"on_ground={self.on_ground}, speed={self.speed})")
 
-    # Example of methods you might add
+
     def update_velocity(self, y_delta, x_delta):
         self.y_velocity += y_delta
         self.x_velocity += x_delta
+        if self.x_velocity > self.max_speed:
+            self.x_velocity = self.max_speed
 
     def set_on_ground(self, on_ground_status):
         self.on_ground = on_ground_status
@@ -25,28 +28,29 @@ class Attribute:
     def set_speed(self, new_speed):
         self.speed = new_speed
 
+
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-player = pygame.Rect((300, 250, 50, 50))
-player = Attribute(y_velocity=0.0, x_velocity=0.0, on_ground=True, speed=0.25, max_speed=1)
+
+player = Attribute(y_velocity=0.0, x_velocity=0.0, on_ground=True, speed=1, max_speed=10,character =pygame.Rect((300, 250, 50, 50)))
 jump = 10
 run = True
 while run:
 
     screen.fill((0,0,0))
 
-    pygame.draw.rect(screen, (255, 0, 0), player)
+    pygame.draw.rect(screen, (255, 0, 0), player.character)
 
     key = pygame.key.get_pressed()
     if (key[pygame.K_a] == True) and (key[pygame.K_d] == True):
         pass
     elif key[pygame.K_a] == True:
-        player.move_ip(-player.speed, 0)
+        player.character.move_ip(-player.speed, 0)
     elif key[pygame.K_d] == True:
-        player.move_ip(player.speed, 0)
+        player.character.move_ip(player.speed, 0)
     if (key[pygame.K_a] == True) and (key[pygame.K_d] == True):
         pass
     elif key[pygame.K_w] == True:
