2be5ace8bcb0499f2d11d9bbc4a4a5ce0d754f78
JhuuJS <joonatan.surakka@edu.hyria.fi>
2024-11-06 13:29:24 +0200

    Update SuperMarkoBrothers.py
    sprtsh

--- super marko brothers/SuperMarkoBrothers.py
+++ super marko brothers/SuperMarkoBrothers.py
@@ -1,5 +1,5 @@
-#the code goes here
 import pygame
+import spritesheet
 pygame.init()
 
 class Attribute:
@@ -11,6 +11,8 @@ class Attribute:
         self.max_speed = max_speed
         self.character = character
 
+
+
     def __repr__(self):
         return (f"Attribute(y_velocity={self.y_velocity}, x_velocity={self.x_velocity}, "
                 f"on_ground={self.on_ground}, speed={self.speed})")
@@ -18,9 +20,9 @@ class Attribute:
 
     def update_velocity(self, y_delta, x_delta):
         self.y_velocity += y_delta
-        self.x_velocity = self.x_velocity / x_delta
-        
-        self.character.move_ip(self.x_velocity,self.y_velocity)
+        self.x_velocity += x_delta
+        if self.x_velocity > self.max_speed:
+            self.x_velocity = self.max_speed
 
     def set_on_ground(self, on_ground_status):
         self.on_ground = on_ground_status
@@ -33,27 +35,41 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Spritesheets')
 
+sprite_sheet_image = pygame.image.load('doux.png').convert_alpha()
+sprite_sheet = spritesheet.SpriteSheet(sprite_sheet_image)
+
+frames = []
+frame_0 = sprite_sheet.get_image(0, 16, 16, 3)
+frames.append(frame_0)
+frame_1 = sprite_sheet.get_image(1, 16, 16, 3)
+frames.append(frame_1)
+frame_2 = sprite_sheet.get_image(2, 16, 16, 3)
+frames.append(frame_2)
+frame_3 = sprite_sheet.get_image(3, 16, 16, 3)
+frames.append(frame_3)
+
+
+clock = pygame.time.Clock()
 
 player = Attribute(y_velocity=0.0, x_velocity=0.0, on_ground=True, speed=1, max_speed=10,character =pygame.Rect((300, 250, 50, 50)))
-jump = -10
+jump = 10
 run = True
-clock = pygame.time.Clock()
 while run:
-
-    player.update_velocity(1.1,1.1)
 
     screen.fill((0,0,0))
 
+    screen.blit(frame_0, (0,  0))
     pygame.draw.rect(screen, (255, 0, 0), player.character)
 
     key = pygame.key.get_pressed()
     if (key[pygame.K_a] == True) and (key[pygame.K_d] == True):
         pass
     elif key[pygame.K_a] == True:
-        player.x_velocity += -player.speed
+        player.character.move_ip(-player.speed, 0)
     elif key[pygame.K_d] == True:
-        player.x_velocity += player.speed
+        player.character.move_ip(player.speed, 0)
     if (key[pygame.K_a] == True) and (key[pygame.K_d] == True):
         pass
     elif key[pygame.K_w] == True:
@@ -62,6 +78,8 @@ while run:
              
     elif key[pygame.K_s] == True:
         pass
+    else:
+        frame_0
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
