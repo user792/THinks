4b9977c604782a496c34bed9aab2c491d2e97b7a
JhuuJS <joonatan.surakka@edu.hyria.fi>
2024-11-07 13:21:26 +0200

    Update level1.py
    hm

--- Super Marko Brothers/level1.py
+++ Super Marko Brothers/level1.py
@@ -2,5 +2,14 @@ import pygame, SuperMarkoBrothers
 pygame.init()
 sand10x = SuperMarkoBrothers.pygame.image.load("materials/sand10x.png").convert_alpha()  
 sand10x = SuperMarkoBrothers.pygame.transform.scale(sand10x,(800,80))
-while True:
+
+    
+rect_1 = pygame.Rect(SuperMarkoBrothers.global_x_offset, 400, 150, 100)
+def collision():
+
+
+
+def funkio():
     SuperMarkoBrothers.screen.blit(sand10x,(SuperMarkoBrothers.global_x_offset,400))
+    pygame.draw.rect(SuperMarkoBrothers.screen, (255, 0, 255), rect_1)
+
